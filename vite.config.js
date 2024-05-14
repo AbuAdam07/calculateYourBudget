aebe565826bc704370a2fc3fc3cf890b369bcfb7
AbuAdam07 <phobosbest@list.ru>
2024-05-14 18:42:25 +0300

    base /

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://abuadam07.github.io/calculateYourBudget/',
+  base: "/",
   plugins: [vue()],
 })
